9300382eb600c48fdc9670f335275f9b3de56923
nfukushim-cj <nfukushima.cj@gmail.com>
2024-03-05 16:53:37 +0000

    [finish]4-12 課題2：アプリケーションを作成してみよう：基礎編

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,20 @@
 class BooksController < ApplicationController
+
   def index
     @books = Book.all
+    @book = Book.new()
   end
 
   def create
     @book = Book.new(book_params)
-
     if @book.save
+     flash[:notice] = "Book was successfully created."
      redirect_to book_path(@book.id)
     else
-      render :new
+      #全ての書籍を再取得する
+      @books = Book.all
+      flash.now[:alert] = "error"
+      render :index
     end
   end
 
@@ -22,12 +27,28 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert] = "error"
+      render :edit
+    end
   end
+
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+     flash[:notice] = "Book was successfully destroyed."
+     redirect_to books_path
+    else
+      flash.now[:alert] = "error"
+      render :index
+    end
+  end
+
   #ストロングパラメータ
-  private
   def book_params
     params.require(:book).permit(:title, :body)
   end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+  #validates :title, presence: true, allow_blank: false
+  #validates :body, presence: true, allow_blank: false
+  validates :title, presence: true
+  validates :body, presence: true
 end
